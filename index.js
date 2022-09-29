7d587e24113019e2efa27e8a297382636dceb844
sjjames020 <sjjames020@gmail.com>
2022-09-29 18:34:28 +0000

    Num2 mutiplesOfThree

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 //Code your solutions in this file
+
+// number 1
 function fiveToOneHundred(){
     for (let i = 5; i <= 100; i++) {
     console.log(i)
     }
 }
 console.log(fiveToOneHundred());
+
+// number 2
+function multiplesOfThree(){
+    for( let i = 1 ; i <= 100; i++){
+        if( i % 3 === 0){
+        console.log(i)}
+    }
+}
+console.log(multiplesOfThree());
