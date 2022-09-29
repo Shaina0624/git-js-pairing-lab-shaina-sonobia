1c985eaa4c8bc2678a60121d58d5ca82328deeb0
sjjames020 <sjjames020@gmail.com>
2022-09-29 19:41:58 +0000

    num10 largest

--- index.js
+++ index.js
@@ -78,3 +78,15 @@ function betweenTwentyAndFourty(num) {
         }
 }
 console.log(betweenTwentyAndFourty(30))
+
+//number 10
+function largest(num1, num2, num3){
+    if (num1 > num2 && num1 > num3) {    
+    return num1
+    }   else if ( num2 > num1 && num2 > num3 ){ 
+    return num2
+      }else 
+      return num3
+}
+
+console.log(largest(600,10,1))
